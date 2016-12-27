b9cda5fb94531b6f5e0953ab3702d8bc12ce7e47
Hanks <zhanghan.me@gmail.com>
2016-12-27 17:16:38 +0800

    [ios] create empty ios project

---
+++ template/ios/WeexVueApp/WeexVueApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeexVueApp
+//
+//  Created by Hanks on 16/12/27.
+//  Copyright © 2016年 Weex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

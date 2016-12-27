76a29a8bfb442a05f951b471e7f019bcdc6df91d
Hanks <zhanghan.me@gmail.com>
2016-12-27 16:55:58 +0800

    add source file

---
+++ template/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="view">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+  import Hello from './components/hello.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      Hello
+    }
+  }
+</script>
+
+<style scoped>
+  .view {
+    width: 100%;
+    height: 100%;
+    background-color: #F5F5F5;
+  }
+</style>
